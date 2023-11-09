2d1ef8de92d0b82673f56eb3119854ad9cc4e690
salamelek <samuel.sluga@gmail.com>
2023-11-09 23:27:39 +0100

    why is it not working pt.2

--- KNN_v1/dataLabelerAuto.py
+++ KNN_v1/dataLabelerAuto.py
@@ -61,7 +61,7 @@ maybe instead of calculating the distance from the line, do the following:
     if the line goes up, only calculate the distance of the values beneath it
     if the line goes down, only calculate the distance of the values above it
 """
-chopMax = 0.01
+chopMax = 0.005
 
 
 with open("GC15min-01-01-23 00:00:00.json", "r") as jsonFile:
@@ -88,9 +88,13 @@ def getDistanceOfPointFromLine(x0, y0, x1, y1, x2, y2):
     :param y2:
     :return:
     """
-    a = np.abs(((x2 - x1) * (y1 - y0)) - ((x1 - x0) * (y2 - y1)))
-    b = np.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
-    return a / b
+    # a = np.abs(((x2 - x1) * (y1 - y0)) - ((x1 - x0) * (y2 - y1)))
+    # b = np.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+    # return a / b
+    numerator = abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
+    denominator = np.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+    distance = numerator / denominator
+    return distance
 
 
 def getChopOfSeries(series):
